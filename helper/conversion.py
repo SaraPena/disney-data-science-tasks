58750bc3032c908448d058eaa19939bc54605ecb
SaraPena <pena.e.sara@gmail.com>
2021-02-22 21:17:46 -0600

    worked with file to help with converting values from string to float.

--- helper/conversion.py
+++ helper/conversion.py
@@ -10,4 +10,17 @@ money_conversion("$790,000") --> 790000
 use test_money_conversion.py to test your solution
 '''
 def money_conversion(money):
-	pass
+    try:
+        budget = float(money.strip('$').split(' million', 1)[0].replace(',',"").split('–',1)[0].split('-')[0])
+            
+        if budget < 500.0:
+            budget =budget * (1000000)
+    
+    except Exception as e:
+            print(e, money)
+            continue
+
+    return budget
+
+        
+    

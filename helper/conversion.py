8fa34178a159ae6cc39d6446ec17ff9a8c3972a8
SaraPena <pena.e.sara@gmail.com>
2021-02-28 16:10:21 -0600

    Creating money conversion functions using video solution.

--- helper/conversion.py
+++ helper/conversion.py
@@ -4,23 +4,44 @@ TODO
 Given either a string or a list of strings as input, return
 a number (int or float) which is equal to the monetary value
 
-money_conversion("$12.2 million") --> 12200000
-money_conversion("$790,000") --> 790000
+money_conversion("$12.2 million") --> 12200000 ## Word syntax
+money_conversion("$790,000") --> 790000 ## value syntax
 
 use test_money_conversion.py to test your solution
 '''
+## Try to get as many cases as possible first and continue to build from there.
+## In many examples there is '$' before the number.
+
+## Seems to be looking for many patters...makes us think regex.
+
+import re
+
+## First looking at the number pattern
+number_pattern = r"\d+(,\d{3})*\.*\d*"
+
+## another pattern we see is amounts such as million, billion, etc.
+
+amounts_pattern = r"thousand|million|billion"
+
+def parse_value_syntax(string):
+    number_pattern = r"\d+(,\d{3})*\.*\d*"
+    value_string = re.search(number_pattern,string).group()
+    value = float(value_string.replace(",",""))
+
+
+    # strip out commas before solution
+    return value
+
+
 def money_conversion(money):
-    try:
-        budget = float(money.strip('$').split(' million', 1)[0].replace(',',"").split('–',1)[0].split('-')[0])
-            
-        if budget < 500.0:
-            budget =budget * (1000000)
-    
-    except Exception as e:
-            print(e, money)
-            continue
+    number_pattern = r"\d+(,\d{3})*\.*\d*"
+    amounts_pattern = r"thousand|million|billion"
 
-    return budget
+    word_pattern = rf"\$(?P<number>{number_pattern})\s(?P<amount>{amounts_pattern})?"
+    value=re.search(word_pattern,money)
 
-        
-    
+    return value
+
+### testing 
+money_conversion("$790,000") 
+parse_value_syntax("$790,000")   
